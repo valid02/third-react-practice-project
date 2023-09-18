6de02eb86d421da552b04fcd8a6ab04511d41e39
Valid Rabiee <validrabieejs@gmail.com>
2023-09-18 14:49:43 +0330

    completing modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import { useState } from "react";
-import UserInput from "./UserInput/UserInput";
-import UsersList from "./UsersLIst/UsersList";
+import { useState } from 'react';
+import UserInput from './UserInput/UserInput';
+import UsersList from './UsersLIst/UsersList';
+import Modal from './Modal/Modal';
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalText, setModalText] = useState('');
 
   const userInputHandler = user => {
-    setUsers(prevUsers => {
-      const updateUsers = [{name: user.username, age: user.age, id: Math.random().toString()}, ...prevUsers];
-      return updateUsers;
-    });
+    if (user.username === '' || user.age === '') {
+      setIsOpen(true);
+      setModalText('please enter a valid name and age (non-empty values).');
+    }
+    else if (user.age < 0) {
+      setIsOpen(true);
+      setModalText('please enter a valid age (> 0).')
+    }
+    else {
+      setUsers(prevUsers => {
+        const updateUsers = [{ name: user.username, age: user.age, id: Math.random().toString() }, ...prevUsers];
+        return updateUsers;
+      });
+      setIsOpen(false);
+    }
+  };
+
+  const clickButtonHandler = () => {
+    setIsOpen(false);
+  }
+
+  const ovarlyClickHandler = event => {
+    if(event.target.id === 'overly') {
+      setIsOpen(false);
+    }
   }
 
   return (
     <div>
       <UserInput user={userInputHandler} />
       <UsersList users={users} />
+      {isOpen && <Modal text={modalText} okButtonClick={clickButtonHandler} overly={ovarlyClickHandler} />}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Modal/Modal.js
@@ -0,0 +1,21 @@
+import styles from './Modal.module.css';
+
+const Modal = props => {
+  return (
+    <div id='overly' className={styles['modal__overly']} onClick={props.overly}>
+      <div className={styles['modal__body']}>
+        <div className={styles['modal__header']}>
+          <p>Invalid input</p>
+        </div>
+        <div className={styles['modal__text']}>
+          <p>{props.text}</p>
+        </div>
+        <div className={styles['modal__btn']}>
+          <button type="button" onClick={props.okButtonClick} >Okay</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
