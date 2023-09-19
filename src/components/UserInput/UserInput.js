dbc80202adac2497a49c38fe5bfed88420546237
Valid Rabiee <validrabieejs@gmail.com>
2023-09-19 14:27:01 +0330

    adding card component

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Card from '../UI/Card';
 import styles from './UserInput.module.css';
 
 const UserInput = props => {
@@ -7,35 +8,32 @@ const UserInput = props => {
 
   const changeUsernameHandler = event => {
     setUsername(event.target.value);
-  }
+  };
 
   const changeAgeHandler = event => {
     setAge(event.target.value);
-  }
+  };
 
   const submitFormHandler = event => {
     event.preventDefault();
 
-    props.user({username: username, age: age});
+    props.user({ username: username, age: age });
 
     setUsername('');
     setAge('');
-  }
+  };
 
-
-  return ( 
-    <div className={styles['input-box']}>
-      <form onSubmit={submitFormHandler} >
-        <div className={styles['input-box__control']}>
-          <label htmlFor='username'>Username</label>
-          <input id='username' type="text" value={username} onChange={changeUsernameHandler} />
-          <label htmlFor='age'>Age (Year)</label>
-          <input id='age' type='number' value={age} onChange={changeAgeHandler} />
-        </div>
-        <button type="submit" className={styles['input-box__btn']}>Add User</button>
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={submitFormHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" value={username} onChange={changeUsernameHandler} />
+        <label htmlFor="age">Age (Year)</label>
+        <input id="age" type="number" value={age} onChange={changeAgeHandler} />
+        <button type="submit" className={styles['input__btn']}>Add User</button>
       </form>
-    </div>
+    </Card>
   );
-}
- 
+};
+
 export default UserInput;
